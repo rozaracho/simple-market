1216b0984fa43bf6acadf3e55c910717df03e3ac
Rosalí <zaracho.rosali@gmail.com>
2020-10-26 08:43:12 -0300

    Se crea proyecto con estructura orientado al dominio

---
+++ src/test/java/com/rozaracho/simplemarket/SimpleMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rozaracho.simplemarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
